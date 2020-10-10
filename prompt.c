7179f351d51e207f3f94545c4f43dca7525e7b01
LeakedMemory <elmq0022@umn.edu>
2020-10-10 12:19:59 -0500

    adds windows and *nix compatibility

--- prompt.c
+++ prompt.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for the user input of size 2048 */
-static char input[2048];
+/* if compile on windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* fake readline function */
+char* readline(char* prompt){
+    fputs(prompt, stdio);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* fake history function */
+void add_history(char* unused) {}
+
+/* otherwise use the readline headers */
+#else
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif
 
 int main(int argc, char** argv){
 
-    /* Print Version and Exit Information */
     puts("Lispy Version 0.0.1");
     puts("Press Ctrl+C to Exit\n");
 
-    /* In a never ending loop */
     while (1) {
 
         /* the prompt */
-        fputs("lispy> ", stdout);
+        char* input = readline("lispy> ");
+        add_history(input);
 
-        /* read a line of user input of maximum size 2048 */
-        fgets(input, 2048, stdin);
-
-        /* echo input back to user */
-        printf("No you're a %s", input);
+        printf("No you're a %s\n", input);
+        free(input);
 
     }
 
