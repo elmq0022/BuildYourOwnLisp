5a69893b2085bd9370473bd17276ea431ef5f799
LeakedMemory <elmq0022@umn.edu>
2020-10-10 16:01:01 -0500

    prints abstract tree if parsed

--- parsing.c
+++ parsing.c
@@ -55,7 +55,17 @@ int main(int argc, char** argv){
         char* input = readline("lispy> ");
         add_history(input);
 
-        printf("No you're a %s\n", input);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            /* on success print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+        } else {
+            /* otherwise print the error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
         free(input);
 
     }
