65d69d8f0ecb4200dcd715a317467ee0609ea575
LeakedMemory <elmq0022@umn.edu>
2020-10-10 13:19:20 -0500

    adds the grammar for the parser

--- parsing.c
+++ parsing.c
@@ -30,6 +30,22 @@ void add_history(char* unused) {}
 
 int main(int argc, char** argv){
 
+    /* create some parsers */
+    mpc_parser_t* Number    = mpc_new("number");
+    mpc_parser_t* Operator  = mpc_new("operator");
+    mpc_parser_t* Expr      = mpc_new("expr");
+    mpc_parser_t* Lispy     = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT,
+            "                                                       \
+            number      : /-?[0-9]+/ ;                              \
+            operator    : '+' | '-' | '*' | '/' ;                   \
+            expr        : <number> | '(' <operator> <expr>+ ')' ;   \
+            lispy       : /^/ <operator> <expr>+ /$/ ;              \
+            ",                                                      \
+            Number, Operator, Expr, Lispy);
+
+
     puts("Lispy Version 0.0.1");
     puts("Press Ctrl+C to Exit\n");
 
@@ -44,5 +60,6 @@ int main(int argc, char** argv){
 
     }
 
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
     return 0;
 }
