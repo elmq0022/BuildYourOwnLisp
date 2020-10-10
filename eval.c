ed0831399b4f30273f1d054260f37949efe14be1
LeakedMemory <elmq0022@umn.edu>
2020-10-10 17:10:01 -0500

    adds basic evaluation

--- eval.c
+++ eval.c
@@ -28,6 +28,33 @@ void add_history(char* unused) {}
 #include <readline/history.h>
 #endif
 
+long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) {return x + y;}
+    if (strcmp(op, "-") == 0) {return x - y;}
+    if (strcmp(op, "*") == 0) {return x * y;}
+    if (strcmp(op, "/") == 0) {return x / y;}
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")){
+        return atoi(t->contents);
+    }
+    
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+}
+
+
+
 int main(int argc, char** argv){
 
     /* create some parsers */
@@ -58,8 +85,10 @@ int main(int argc, char** argv){
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
             /* on success print the AST */
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%li\n", result);
             mpc_ast_delete(r.output);
+
         } else {
             /* otherwise print the error */
             mpc_err_print(r.error);
